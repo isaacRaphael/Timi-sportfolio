620257391fa7cbec224c5bc7e3bec252cab94910
Raphael Isaac <isaacraphaelchidi@gmail.com>
2022-07-07 16:25:49 +0100

    text change

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -56,7 +56,7 @@ const Homepage = ({ handlePageChange }) => {
                     <div className='w-full h-1/2 bg-[#EBF5FF] flex flex-col justify-between py-4'>
                         <div className='px-8 text-left'>
                             <h1 className='text-[24px] xl:text-[16px]'><span className='font-semibold'>Pet Me</span>-  An online petshop.</h1>
-                            <p className='mt-2 text-[16px] text-[#4d4d4d] xl:text-[14px]'>A market place where merchants go to list their products or services for sale.</p>
+                            <p className='mt-2 text-[16px] text-[#4d4d4d] xl:text-[14px]'>A market place that connects pet owners to premium pet products and services.</p>
                         </div>
                         <a href='https://medium.com/@adepojutimilehin7/fellowship-at-the-bulb-africa-examination-portal-a-case-study-6462ed68943b' className='px-8 py-12 text-left flex'>
                             <h1 className='text-[16px] font-semibold xl:text-[14px]'>View Case study</h1>
@@ -74,7 +74,7 @@ const Homepage = ({ handlePageChange }) => {
                             <p className='mt-2 text-[16px] text-[#4d4d4d] xl:text-[14px]'>Helping users shop for quality thrift wears from the comfort of their homes.</p>
                         </div>
                         <div className='px-8 py-12 text-left flex'>
-                            <h1 className='text-[16px] font-semibold xl:text-[14px]'>Coming Soon</h1>
+                            <h1 className='text-[16px] font-semibold xl:text-[14px]'>View Case Study</h1>
                             <img className='ml-4' src="./chevron.png" alt="button" />
 
                         </div>
